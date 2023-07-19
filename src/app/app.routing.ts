b799fb9be4ee8751ce4739ebe155e055f96a6ea7
César Ramírez <cesar.ramirez1@outlook.com>
2023-07-19 11:53:12 -0500

    fix: routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { Route } from "@angular/router";
 
 export const appRoutes: Route[] = [
   { path: '', pathMatch: 'full', redirectTo: 'auth/sign-in' },
+  { path: 'sign-in', pathMatch: 'full', redirectTo: 'auth/sign-in' },
   {
     path: '',
     children: [
@@ -9,5 +10,6 @@ export const appRoutes: Route[] = [
       { path: 'products', loadChildren: () => import('src/app/modules/products/products.module').then(m => m.ProductsModule) },
       { path: 'cart', loadChildren: () => import('src/app/modules/cart/cart.module').then(m => m.CartModule) },
     ],
-  }
+  },
+  { path: '**', redirectTo: 'auth/sign-in' },
 ];
